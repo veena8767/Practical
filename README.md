1511186caaa88a4472cc8b26db44a47fd8325cae
diyaaa-s <diyaa.senthilkumar@gmail.com>
2025-05-19 11:57:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Practical
+# cpgm
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n1,n2,sum;
+printf("Enter two integers");
+scanf("%d %d, &n1,&n2);
+sum=n1+n2;
+printf("&d +&d=&sum",n1,n2,sum);
+return 0;
+}
---
+++ add.txt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h> 
+int main() 
+{ int n1,n2,sum; 
+ printf("Enter two integers"); 
+ scanf("%d %d, &n1,&n2); 
+ sum=n1+n2; printf("&d +&d=&sum",n1,n2,sum); 
+ return 0;
+end
+}
+#first committtt
+#firsttttt 
+
+ 
