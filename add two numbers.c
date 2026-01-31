911b7cb7197235a1a0e8a46120af70e13bdc0076
veena8767 <veena.vee879@gmail.com>
2026-01-31 11:06:29 +0530

    Update add two numbers.c

--- add two numbers.c
+++ add two numbers.c
@@ -2,8 +2,9 @@
 #include<conio.h> 
 int main() 
 { int n1,n2,sum; 
- printf("Enter two integers"); 
+ printf("Enter 2 nos"); 
  scanf("%d %d, &n1,&n2); 
  sum=n1+n2; printf("&d +&d=&sum",n1,n2,sum); 
  return 0;
 }
+
